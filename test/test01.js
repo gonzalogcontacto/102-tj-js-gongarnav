7fb7590708f74f8e3c1871293db1516e62ea840e
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-05-04 13:58:06 +0200

    * test01 passed

--- test/test01.js
+++ test/test01.js
@@ -1,5 +1,20 @@
 function numPrimo(input) {
- return null;
+    if(input == 1){ return -1}
+    return getPrimes(input);
+}
+
+function getPrimes(max) {
+    var sieve = [], i, j, primes = [];
+    for (i = 2; i <= max; ++i) {
+        if (!sieve[i]) {
+            // i has not been marked -- it is prime
+            primes.push(i);
+            for (j = i << 1; j <= max; j += i) {
+                sieve[j] = true;
+            }
+        }
+    }
+    return primes;
 }
 
 module.exports = numPrimo;
